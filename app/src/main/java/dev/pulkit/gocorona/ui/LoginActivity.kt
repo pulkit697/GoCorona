759b2d33fa1ca1a73e9d7efa412c1597548521f7
Pulkit-Windows <pulkitgarg697@gmail.com>
2021-06-25 02:15:46 +0530

    login activity + icon

--- app/src/main/java/dev/pulkit/gocorona/ui/LoginActivity.kt
+++ app/src/main/java/dev/pulkit/gocorona/ui/LoginActivity.kt
@@ -1,13 +1,20 @@
 package dev.pulkit.gocorona.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.pulkit.gocorona.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btLoggedIn.setOnClickListener {
+            Intent(this,MainActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
