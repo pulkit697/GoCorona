d88da986439c6a2f5257b9d3d0839a6fa169faeb
Pulkit-Windows <pulkitgarg697@gmail.com>
2021-08-22 02:06:48 +0530

    ui changes

--- app/src/main/java/dev/pulkit/gocorona/ui/MainActivity.kt
+++ app/src/main/java/dev/pulkit/gocorona/ui/MainActivity.kt
@@ -1,18 +1,27 @@
 package dev.pulkit.gocorona.ui
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.pulkit.gocorona.R
 import dev.pulkit.gocorona.models.ctscan.Comment
+import dev.pulkit.gocorona.ui.fragments.HomeFragment
+import dev.pulkit.gocorona.ui.fragments.XRayScanFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_x_ray_scan.*
+import android.content.res.ColorStateList
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,18 +30,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         navController = fragMainActivity.findNavController()
+        bnb_main.background = null
+        bnb_main.itemIconTintList = ColorStateList(
+            arrayOf(
+                intArrayOf(-android.R.attr.state_checked),
+                intArrayOf(android.R.attr.state_checked)
+            ), intArrayOf(
+                Color.parseColor("#000000"),
+                Color.parseColor("#ffffff")
+            )
+        )
         bnb_main.setupWithNavController(navController)
         val listOfFragsWithNavBar = listOf(R.id.homeFragment,R.id.XRayReviewFragment,R.id.XRayScanFragment,R.id.aboutFragment)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id in listOfFragsWithNavBar){
-                bnb_main.visibility = View.VISIBLE
+                cl_bottom_bar_main.visibility = View.VISIBLE
+                if(destination.id == R.id.XRayScanFragment){
+                    fabBottomBar.setImageResource(R.drawable.xray_white)
+                    fabBottomBar.setColorFilter(Color.WHITE)
+                }else{
+                    fabBottomBar.setImageResource(R.drawable.x_ray_black)
+                }
             }else{
-                bnb_main.visibility = View.GONE
+                cl_bottom_bar_main.visibility = View.GONE
             }
         }
     }
+    fun navigateToXrayScanFragment(view: View){
+        bnb_main.uncheckAllItems()
+        val navHostFragment = supportFragmentManager.primaryNavigationFragment
+        val fragment = navHostFragment?.childFragmentManager?.fragments?.get(0)
+        if (fragment is HomeFragment){
+            navController.navigate(R.id.action_homeFragment_to_XRayScanFragment)
+        }else{
+            navController.navigate(R.id.action_aboutFragment_to_XRayScanFragment)
+        }
+    }
+
     fun navigateToHistoryFragment(view:View) {
         navController.navigate(R.id.action_XRayScanFragment_to_historyMyXRaysFragment)
     }
@@ -62,5 +97,18 @@ class MainActivity : AppCompatActivity() {
             data = Uri.parse("https://www.pmcares.gov.in/en/")
             startActivity(this)
         }
+    }
+    fun registerVaccine(view:View){
+        Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("https://selfregistration.cowin.gov.in/selfregistration")
+            startActivity(this)
+        }
+    }
+    fun BottomNavigationView.uncheckAllItems() {
+        menu.setGroupCheckable(0, true, false)
+        for (i in 0 until menu.size()) {
+            menu.getItem(i).isChecked = false
+        }
+        menu.setGroupCheckable(0, true, true)
     }
 }
